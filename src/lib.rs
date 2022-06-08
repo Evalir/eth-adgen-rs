5688ca511b509a83853a3dafae1d3eb1152c8dfb
Enrique Ortiz <hi@enriqueortiz.dev>
2022-06-08 18:51:39 -0400

    [WIP] Separate `binary` and `lib` + generalize address creation (#2)
    * feat: refactor addr generation into function and struct
    
    * feat: conform to parity types for ethers-rs compat
    
    * chore: use proper public key type (h520)
    
    * feat: use secret/public type abbrvs
    
    * chore: separate lib and binary
    
    * feat: add initial cli structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+use ethereum_types::{Address, Public, Secret};
 use k256::{ecdsa::SigningKey, elliptic_curve::sec1::ToEncodedPoint, PublicKey};
 use tiny_keccak::{Hasher, Keccak};
+
+pub struct Account {
+    // Raw private key
+    pub private_key: Secret,
+    // Raw, public key (stripped of the 0x04 lead byte)
+    pub public_key: Public,
+    // Ethereum address
+    pub address: Address,
+}
 
 pub fn keccak256<S>(bytes: S) -> [u8; 32]
 where
@@ -12,20 +22,20 @@ where
     output
 }
 
-fn main() -> eyre::Result<()> {
+pub fn generate_random() -> Account {
     let mut rng = rand::thread_rng();
 
     let private_key = SigningKey::random(&mut rng);
     let public_key = PublicKey::from(&private_key.verifying_key()).to_encoded_point(false);
     let public_key = public_key.as_bytes();
-    // make sure to check the leading byte is 0x04 for the uncompressed public key
+    // make sure to check the leading byte is 0x04 for uncompressed coordinates
     debug_assert_eq!(0x04, public_key[0]);
     // then skip it for address calculation
     let addr = keccak256(&public_key[1..]);
 
-    println!("private key: 0x{}", hex::encode(private_key.to_bytes()));
-    println!("public key: 0x{}", hex::encode(&public_key[1..]));
-    println!("addr: 0x{}", hex::encode(&addr[12..]));
-
-    Ok(())
+    Account {
+        private_key: Secret::from_slice(&private_key.to_bytes()),
+        public_key: Public::from_slice(&public_key[1..]),
+        address: Address::from_slice(&addr[12..]),
+    }
 }
